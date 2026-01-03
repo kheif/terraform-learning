39ac31ef44cb1f4102c5825fecaba1390bae6b84
kheif <aydinmehmetege@gmail.com>
2026-01-03 10:48:21 +0100

    refactor: restructure project layout and move main tf files

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "repo_name" {
-  description = "Name of the created repository"
-  value       = github_repository.new.name
-}
-
-output "repo_html_url" {
-  description = "HTML URL of the created repository"
-  value       = github_repository.new.html_url
-}
