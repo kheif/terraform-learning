394e6b64fbd9b7d39ea5524fd4f2ce910dd0816b
kheif <aydinmehmetege@gmail.com>
2026-01-04 23:52:55 +0100

    integrate powerShell script using terraform output variables

---
+++ terraform-basics/greeting.ps1
@@ -0,0 +1,6 @@
+$recieved_name = $args[0]
+
+Write-Host "----------------------------------------" -ForegroundColor Green
+Write-Host "SYSTE: NEW GREETINGS MODULE ACTIVATED" -ForegroundColor Yellow
+Write-Host "Hello, $recieved_name! Welcome to our system." -ForegroundColor Cyan
+Write-Host "----------------------------------------" -ForegroundColor Green
